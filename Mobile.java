a1553cbcab37d9a1ca62554c58cfd5d4dc03b86b
Ranj-in21 <108992272+Ranj-in21@users.noreply.github.com>
2022-07-09 21:50:48 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Mobile {
+	public void makeCall()
+	{
+		// TODO Auto-generated method stub
+		String name = "Make a call to beauty salon";
+		System.out.println("Hi Alexa," +name);
+
+	}
+	public void sendMsg() 
+	{
+		// TODO Auto-generated method stub
+		long number = 999999999;
+		System.out.println("Hi Alexa, Make a call to" +number);
+
+	}	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hi, This is my mobile");
+	}
+
+}
